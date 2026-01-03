869d5567b1e2632436eabd05fda89659caad9166
Claus Marvin N. Hipolito <217520849+maru395@users.noreply.github.com>
2026-01-03 21:32:26 +0800

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,11 +1,12 @@
 def resolution_streak(days):
+    #requirements
     min_steps = 1000
     max_screen = 60
     min_pages = 10
 
     streak = 0
-    for day in day:
-        step, screen, page = day
-        if step >= min_steps and screen <= max_screen and page >= min_pages:
+    for day in days:
+        step, screen, page = day #initializes the day[0], day[1], day[2]
+        if step >= min_steps and screen <= max_screen and page >= min_pages: 
             streak += 1
     return streak
