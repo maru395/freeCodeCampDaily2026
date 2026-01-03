976a669ff33d83b2f37b4c17c6291a75a47306fc
Claus Marvin N. Hipolito <217520849+maru395@users.noreply.github.com>
2026-01-03 21:27:02 +0800

    Update day3.py

--- day3.py
+++ day3.py
@@ -1,8 +1,8 @@
 def find_left_handed_seat(table):
     available = 0
-    for seat in table:
-      for i in range(len(seat)):
-          if seat[i] == "U":
-              if i + 1 >= len(seat) or seat[i + 1] != "R":
+    for seat in table: #access elements at the table one by one
+      for i in range(len(seat)): #access the index of each element
+          if seat[i] == "U": 
+              if i + 1 >= len(seat) or seat[i + 1] != "R": #first checks if avaible seat has no seat on its right, if it has a seat then checks if the seat next to it is right handed
                 available += 1
     return available
