347a60ce2592a53d06562aa14396d097eed814f4
Claus Marvin N. Hipolito <217520849+maru395@users.noreply.github.com>
2026-02-19 20:56:23 +0800

    Create day19.py

---
+++ feb/day19.py
@@ -0,0 +1,10 @@
+def avalanche_risk(snow_depth, slope):
+    return "Safe" if snow_depth == "Shallow" or slope == "Gentle" else "Risky"
+
+    # the idea
+    # if snow_depth == "Shallow":
+    #     return "Safe"
+    # elif slope == "Gentle":
+    #     return "Safe"
+    # else:
+    #     return "Risky"
