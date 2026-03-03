891d3052b5e7353ed432a25d36e26e214e0a9219
Claus Marvin N. Hipolito <217520849+maru395@users.noreply.github.com>
2026-03-03 17:50:53 +0800

    Update day3.py

--- mar/day3.py
+++ mar/day3.py
@@ -3,3 +3,4 @@ import math
 def count_perfect_cubes(a, b):
     return sum((math.cbrt(i)).is_integer() for i in range(a, b + (-1 if a > b else 1), -1 if a > b else 1))
 # for faster way we can use mathematical formula for checking the perfect cubes in a range: $$\text{Count} = \lfloor \sqrt[3]{\text{end}} \rfloor - \lceil \sqrt[3]{\text{start}} \rceil + 1$$
+# count = max(0, math.floor(math.cbrt(b - 1)) - math.ceil(math.cbrt(a)) + 1)
