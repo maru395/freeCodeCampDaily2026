1c94caf144ff12a8339ae2ee82d82a81be278815
Claus Marvin N. Hipolito <217520849+maru395@users.noreply.github.com>
2026-05-12 08:05:02 +0800

    Update day11.py

--- may/day11.py
+++ may/day11.py
@@ -1,4 +1,20 @@
+# c optimized way is data is static
 def get_oldest(people):
     age = max(p['age'] for p in people)
     return [p['name'] for p in people if p['age'] == age]
-# still slow tho
+# if dynamic
+# def get_oldest(people):
+#     if not people: return []
+    
+#     max_age = -1
+#     oldest_names = []
+    
+#     for p in people:
+#         age = p['age']
+#         if age > max_age:
+#             max_age = age
+#             oldest_names = [p['name']] # Found a new leader, reset list
+#         elif age == max_age:
+#             oldest_names.append(p['name']) # It's a tie
+            
+#     return oldest_names
