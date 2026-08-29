b911dd28642d677135f9153d16cb61613f4ad75e
Claus Marvin N. Hipolito <217520849+maru395@users.noreply.github.com>
2026-08-29 21:03:40 +0800

    Create day14.py

---
+++ july/day14.py
@@ -0,0 +1,11 @@
+def pet_years(pet, age):
+    multiplier = {
+        "dog": 7,
+        "cat": 6,
+        "rabbit": 8,
+        "hamster": 30,
+        "guinea pig": 12,
+        "goldfish": 6,
+        "bird": 5
+    }
+    return age * multiplier[pet]
